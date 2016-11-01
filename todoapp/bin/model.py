d7f548a55a6e3d56f80cb67fffd0d1cf73de37bb
Diego Arrieta <diego.arrieta.r@gmail.com>
2016-11-01 08:10:54 -0600

    web exercise made with webpy

---
+++ todoapp/bin/model.py
@@ -0,0 +1,12 @@
+import web
+
+db = web.database(dbn='postgres', db='todo', user='diego', pw='gormo')
+
+def get_todos():
+    return db.select('todo', order='id')
+
+def new_todo(text):
+    db.insert('todo', title=text)
+
+def del_todo(id):
+    db.delete('todo', where="id=$id", vars=locals())
---
+++ todoapp/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setuptools
+
+config  = {
+    'description':'My project',
+    'author':'Diego Arrieta',
+    'url':'myproject.com',
+    'download_url':'Where to download it.',
+    'author_email':'diego.arrieta.r@gmail.com',
+    'version':'0.1',
+    'install_requires':['nose'],
+    'packages':['NAME'],
+    'scripts':[],
+    'name':'projectname'
+}
+
+setup(**config)
